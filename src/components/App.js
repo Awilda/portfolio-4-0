d067e0e3b747b188ed8ce936650bdd084f9d8f6b
Carolina de la Fuente <mdela160@fiu.edu>
2017-06-06 20:42:57 -0400

    messing with css positioning

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,18 @@ import React, { Component } from 'react'
 
 class App extends Component {
   render () {
-    return <div>
-      <h1>Hello, World!</h1>
+    return <div className='App'>
+      <div className='menu'>
+        <ul>
+          <li> Home </li>
+          <li> Projects </li>
+          <li> Contact </li>
+        </ul>
+      </div>
+      <div className='image'>
+        <h2> Carolina de la Fuente </h2>
+        <img src='../albert-dera-245502.jpg' width='500px' />
+      </div>
     </div>
   }
 }
