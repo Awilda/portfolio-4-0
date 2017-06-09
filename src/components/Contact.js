3ca4b187d007f5f732cb44ae9bfa97a7e7049d34
Carolina de la Fuente <mdela160@fiu.edu>
2017-06-09 10:59:31 -0400

    added brewtab project

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Contact extends Component {
   render () {
     return <div className='Contact slide'>
-      <h3> Let's Connect </h3>
+      <h3> Let's Connect. </h3>
       <a target='_blank' href='mailto:mdela160@fiu.edu'> E-mail </a>
       <a target='_blank' href='https://github.com/carodelafuente/'> Github </a>
       <a target='_blank' href='https://www.instagram.com/carohhh/'> Instagram </a>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,6 +5,11 @@ class Projects extends Component {
     return <div className='Projects slide'>
       <h3> My Projects </h3>
       <div className='projectBox'>
+        <img src='../brewtab.png' width='300px' />
+        <span> An mobile web app that allows you to keep a 'tab' of all the beers you've tried and saves your favorites too. Built with React and Sass and uses libraries such as Auth0 and React Router.</span>
+        <a href='http://brewtab.carodelafuente.surge.sh/'> Click to view it live. </a>
+      </div>
+      <div className='projectBox'>
         <img src='../memory.png' width='300px' />
         <span> A memory game written with React and styled with Sass.</span>
         <a href='http://memory.carodelafuente.surge.sh/'> Click to view it live. </a>
