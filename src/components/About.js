716f4fe9baf8e889285ea9de8a3e4e9430928ed4
Carolina de la Fuente <carolina.delafuente@theironyard.com>
2017-10-30 12:02:43 -0400

    Fixed up my about section

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ class About extends Component {
       <h3> Call me Carolina. </h3>
       {/* for later: CSS grids used in Soma project to put pics */}
       {/* <img src='../avatar.png' width='150px' /> */}
-      <span> Full name: Mirza Carolina de la Fuente, but call me Carolina! I'm a Junior Frontend Developer from Miami, currently working in Washington, DC as an Associate Instructor at The Iron Yard, a 12-week intensive coding bootcamp. I'm passionately curious about tech, media, and design. Where those 3 points meet is where I want to land. In my spare time, I like to find new spots around town and drink lots of smoothies. </span>
+      <span> Full name: Mirza Carolina de la Fuente, but call me Carolina! I'm a Junior Frontend Developer from Miami, currently working in Washington, DC as an Associate Instructor at The Iron Yard, a 12-week intensive coding bootcamp. I'm passionately curious about tech and design. Where those meet is where I want to land. In my spare time, I like to find new spots around town and drink lots of smoothies. </span>
     </div>
   }
 }
