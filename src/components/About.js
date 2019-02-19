b4ec8fcb6653044295038ab35d650122cd3a7594
Awilda I Couverthier <awildacouverthier@gmail.com>
2019-02-19 13:16:23 -0500

    add portfolio content

--- src/components/About.js
+++ src/components/About.js
@@ -5,13 +5,13 @@ class About extends Component {
     return <div className='About'>
     <div className="aboutContainer">
     <div className="aboutTextContainer">
-      <h3>Carolina de la Fuente</h3>
-      <p>She/her. Latinx.</p>
-      <p>Peru-born. Miami raised. Brooklyn living.</p>
-      <p>Retired college DJ. Aspiring Web Wizard.</p>
+      <h3>Nice to meet you!</h3>
+      <p>I'm a New York City based Web Developer but haven't always been a developer. I've worked in hospitals, theatres and retail stores yet somehow all that has led me here. It's a good thing I enjoy learning new things because what a journey it has been!</p>
+      <p>In my free time, I enjoy cuddling with my fur-babies, reading, and  taking up a new gym class.</p> 
+      <p>I hope to one day use my skills as a programmer to give back and contribute to the greater good.</p>
       </div>
       <div className="aboutImageContainer">
-      <img src="../mefilm.jpeg" />
+      <img src="../aic2.jpeg" />
       </div>
       </div>
       </div>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,11 +3,10 @@ import React, { Component } from 'react'
 class Contact extends Component {
   render () {
     return <div className='Contact slide'>
-      <h4>Let's Connect.</h4>
-      <p><a className="contact_link" target='_blank' href='mailto:dmirzacarolina@gmail.com'> dmirzacarolina@gmail.com </a></p>
-      <p><a className="contact_link" target='_blank' href='https://github.com/carodelafuente/'> Github </a></p>
-      <p><a className="contact_link" target='_blank' href='https://www.linkedin.com/in/mirzacarolina/'> LinkedIn </a></p>
-      <p><a className="contact_link" target='_blank' href='https://www.instagram.com/ojosnublados/'> Photography IG </a></p>
+      <h4>Connect with me :)</h4>
+      <p><a className="contact_link" target='_blank' href='mailto:awildacouverthier@gmail.com'> Email Me! </a></p>
+      <p><a className="contact_link" target='_blank' href='https://github.com/awilda'> Check out my Github </a></p>
+      <p><a className="contact_link" target='_blank' href='https://www.linkedin.com/in/awildacouverthier/'> Visit me on LinkedIn </a></p>
     </div>
   }
 }
